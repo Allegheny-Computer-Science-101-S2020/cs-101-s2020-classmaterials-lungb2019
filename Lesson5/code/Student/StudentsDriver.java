0c508ef5990d763c5f552026c896324288697778
Bernice Lung <bernicelung@BernicedeMacBook-Pro.local>
2020-02-18 10:45:52 -0500

    Lesson5

--- Lesson5/code/Student/StudentsDriver.java
+++ Lesson5/code/Student/StudentsDriver.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class StudentsDriver{
 	public void loadStudents(Students[] obj){
 		/* add logic here */
-	}	
+		for (int i = 0; i < obj.length; i++){
+			obj[i] = new Students();
+			obj[i].setStudentID(i+1);
+			obj[i].setStudentGPA(4*i/5);
+			obj[i].setStudentName("S" + i);
+		}
+	}
 	public void displayStudents(Students[] obj){
 		/* add logic here */
-	}	
+		for (int i = 0; i < obj.length; i++){
+			System.out.println(obj[i].getStudentID()+ "\t" +
+				obj[i].setStudentGPA()+ "\t" +
+				obj[i].setStudentName()+ "\t" +
+		}
+	}
 }
